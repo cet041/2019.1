4a38ed235b174a0cd124b3af48cb198c8eb2b4a7
Mathias Santos de Brito <mathias.brito@me.com>
2019-04-06 11:39:16 -0300

    exercicios despesas e idade

---
+++ exercicios/depesas_e_receitas/despesas_e_receitas.c
@@ -0,0 +1,11 @@
+/*Crie um programa que leia (scanf) do usuário o seu salário, os seus gastos e
+ * imprima (printf) na tela 1 se ele for poupador e 2 se ele for gastão. Ele
+ * será rico se a subtração do seu salário pelas suas despesas resultar em um
+ * valor maior que 200.*/
+#include <stdio.h>
+
+int main() {
+    //código aqui!
+    return 0;
+}
+
